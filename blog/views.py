636418f43a9c4e0b2c8fcb39d65ec30c53773269
Rocer23 <ip314338@gmail.com>
2024-12-29 18:34:27 +0200

    Added DataBase

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    context = {
+        "posts_list": posts,
+    }
+    return render(
+        request,
+        "blog/post_list.html",
+        context=context
+    )
+
+
+def get_post_by_id(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {
+        "post": post,
+    }
+    return render(
+        request,
+        "blog/post_detail.html",
+        context=context
+    )
